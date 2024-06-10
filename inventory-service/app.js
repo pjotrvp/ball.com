a04bacd8927a7c9014d68f53aec10a3f799cc434
Mees Verhelpen <mees.verhelpen@gmail.com>
2024-06-10 19:38:53 +0200

    feat(inventory-service): add docker image

--- inventory-service/app.js
+++ inventory-service/app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 9090
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Inventory service initiated on port ${port}`)
 })
