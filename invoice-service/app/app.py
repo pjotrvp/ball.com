af1a122fccf1359721f687e3fd1d4913c4fbb582
Matthijs07g <matthijs07g@gmail.com>
2024-06-11 23:34:20 +0200

    refactoring to invoice-service

--- invoice-service/app/app.py
+++ invoice-service/app/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/api', methods=['GET'])
+@app.route('/', methods=['GET'])
 def hello_world():
     return jsonify({"message": "Hello, World!"})
 
