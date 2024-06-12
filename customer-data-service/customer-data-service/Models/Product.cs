9fe15c943107dd350b5cd0669d6999c718c3bea7
daviddierckx <62950437+daviddierckx@users.noreply.github.com>
2024-06-12 15:55:42 +0200

    Added data service

---
+++ customer-data-service/customer-data-service/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace customer_data_service.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
