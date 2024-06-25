9994eb943d417868a8c934c0ffd28e7c7b41755d
Matthijs07g <matthijs07g@gmail.com>
2024-06-25 16:04:21 +0200

    attempt at multiple db connections

--- invoice-service/app/__init__.py
+++ invoice-service/app/__init__.py
@@ -5,25 +5,22 @@ from flask_sqlalchemy import SQLAlchemy # type: ignore
 from sqlalchemy.exc import OperationalError # type: ignore
 import time
 
-
 db = SQLAlchemy()
 
 def create_app():
     logging.basicConfig(level=logging.DEBUG)
     logger = logging.getLogger(__name__)
 
-
     app = Flask(__name__)
     app.config.from_object('app.config.Config')
 
-    logger.debug("Initializing database...")
+    logger.debug("Initializing databases...")
     db.init_app(app)
-    
+
     from app.controller.invoice_controller import invoice_blueprint
     from app.controller.message_controller import message_blueprint
 
-    logger.debug("Registring blueprints...")
-    # Register all blueprints
+    logger.debug("Registering blueprints...")
     app.register_blueprint(invoice_blueprint, url_prefix='/invoice')
     app.register_blueprint(message_blueprint, url_prefix='/message')
 
@@ -34,12 +31,21 @@ def create_app():
 def initialize_database(app):
     logging.basicConfig(level=logging.DEBUG)
     logger = logging.getLogger(__name__)
+    
     with app.app_context():
         retry_attempts = 5
         for attempt in range(retry_attempts):
             try:
-                db.create_all()
-                logger.info("Successfully created all database tables and connected to the database.")
+                # Create tables for read database
+                with db.get_engine(app, bind='read').connect() as conn:
+                    db.Model.metadata.create_all(bind=conn)
+                logger.info("Successfully created all read database tables and connected to the database.")
+
+                # Create tables for write database
+                with db.get_engine(app).connect() as conn:
+                    db.Model.metadata.create_all(bind=conn)
+                logger.info("Successfully created all write database tables and connected to the database.")
+                
                 break
             except OperationalError as e:
                 logger.error(f"Failed to connect to the database (attempt {attempt + 1}/{retry_attempts}): {e}")
@@ -47,6 +53,7 @@ def initialize_database(app):
                     time.sleep(5)  # Wait before retrying
                 else:
                     raise
+
 
 def connect_to_rabbitmq():
     logging.basicConfig(level=logging.DEBUG)
